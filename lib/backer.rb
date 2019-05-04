480e72d8e303fa2100c28b5862527ddda1de119b
roy renno <royrenno@yahoo.com>
2019-05-04 00:40:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,13 @@
 class Backer
   attr_accessor :name, :backed_projects
+
   def initialize(name)
     @name = name
     @backed_projects = []
   end
+  
   def back_project(project)
-    @back_project<<project
-    project.add_backer(self)unless project.backer.include?(self)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
   end
 end
